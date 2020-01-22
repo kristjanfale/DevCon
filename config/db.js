305bd33a945652d77fd681861f3896ad1d209505
Kristjan Fale <kristjan.fale@gmail.com>
2020-01-22 16:21:39 +0100

    Connect to MongoDB with Mongoose

---
+++ config/db.js
@@ -0,0 +1,20 @@
+// Connect server to MongoDB
+const mongoose = require('mongoose');
+
+const config = require('config'); // Get access to global variables in config/default.json
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }); // Connect to MongoDB
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
